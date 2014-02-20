50986f6c35695995bf0c987e1c2fb4e74a4e3d5a
nham <hamann.nick@gmail.com>
2014-02-20 08:11:31 -0500

    use modules

--- tictactoe.rs
+++ tictactoe.rs
@@ -1,184 +1,213 @@
-use std::rand;
-use std::rand::distributions::{IndependentSample, Range};
 use std::hashmap::HashMap;
-use std::fmt::{Show, Formatter};
-use std::fmt;
 
-#[deriving(Eq)]
-enum Cell {
-    Nil,
-    X,
-    O,
-}
+use game::Game;
+use player::{Player, RandomPlayer, RLPlayer, PX, PO};
 
-impl Show for Cell {
-    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
-        let s = match *self {
-                    Nil => " ",
-                    X   => "X",
-                    O   => "O",
-        };
+mod game {
+    use std::fmt::{Show, Formatter};
+    use std::fmt;
 
-        write!(f.buf, "{:s}", s)
-    }
-}
-
-impl Show for [Cell, ..3] {
-    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
-        write!(f.buf, "|{} {} {}|", self[0], self[1], self[2])
-    }
-}
-
-enum PlayerType {
-    PX,
-    PO,
-}
-
-type GameState = [[Cell, ..3], ..3];
-
-struct Game {
-    state: GameState,
-}
-
-impl Game {
-    fn new() -> Game {
-        Game { state: [[Nil, Nil, Nil], [Nil, Nil, Nil], [Nil, Nil, Nil]] }
+    #[deriving(Eq)]
+    pub enum Cell {
+        Nil,
+        X,
+        O,
     }
 
-    fn get(&self, x: uint, y: uint) -> Result<Cell, &str> {
-        if !(x >= 1 && x <= 3 && y >= 1 && y <= 3 ) {
-            Err("Out of range")
-        } else {
-            Ok( self.state[x-1][y-1] )
+    impl Show for Cell {
+        fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+            let s = match *self {
+                        Nil => " ",
+                        X   => "X",
+                        O   => "O",
+            };
+
+            write!(f.buf, "{:s}", s)
         }
     }
 
-    fn cell_is_avail(&self, x: uint, y: uint) -> bool {
-        let res = self.get(x,y);
-        match self.get(x,y) {
-            Ok(Nil) => true,
-            _       => false,
+    impl Show for [Cell, ..3] {
+        fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+            write!(f.buf, "|{} {} {}|", self[0], self[1], self[2])
         }
     }
 
-    fn set(&mut self, x: uint, y: uint, c: Cell) -> bool {
-        if !(x >= 1 && x <= 3 && y >= 1 && y <= 3 ) {
+
+    pub type GameState = [[Cell, ..3], ..3];
+
+    pub struct Game {
+        state: GameState,
+    }
+
+    impl Game {
+        pub fn new() -> Game {
+            Game { state: [[Nil, Nil, Nil], [Nil, Nil, Nil], [Nil, Nil, Nil]] }
+        }
+
+        pub fn get(&self, x: uint, y: uint) -> Result<Cell, &str> {
+            if !(x >= 1 && x <= 3 && y >= 1 && y <= 3 ) {
+                Err("Out of range")
+            } else {
+                Ok( self.state[x-1][y-1] )
+            }
+        }
+
+        pub fn cell_is_avail(&self, x: uint, y: uint) -> bool {
+            let res = self.get(x,y);
+            match self.get(x,y) {
+                Ok(Nil) => true,
+                _       => false,
+            }
+        }
+
+        pub fn set(&mut self, x: uint, y: uint, c: Cell) -> bool {
+            if !(x >= 1 && x <= 3 && y >= 1 && y <= 3 ) {
+                false
+            } else {
+                self.state[x-1][y-1] = c;
+                true
+            }
+        }
+
+        pub fn print(&self) {
+            println!("{}\n{}\n{}", self.state[0], self.state[1], self.state[2]);
+        }
+
+        fn is_won(&self) -> bool {
+            for i in range(1u, 4u) {
+                if self.get(i, 1u) == self.get(i, 2u)
+                && self.get(i, 2u) == self.get(i, 3u)
+                && self.get(i, 1u) == self.get(i, 3u) {
+                    match self.get(i, 1u) {
+                        Ok(X) => return true,
+                        Ok(O) => return true,
+                        _ => (),
+                    }
+                }
+            }
+
+            for i in range(1u, 4u) {
+                if self.get(1u, i) == self.get(2u, i)
+                && self.get(2u, i) == self.get(3u, i)
+                && self.get(1u, i) == self.get(3u, i) {
+                    match self.get(1u, i) {
+                        Ok(X) => return true,
+                        Ok(O) => return true,
+                        _ => (),
+                    }
+                }
+            }
+
+            if self.get(1u, 1u) == self.get(2u, 2u)
+            && self.get(2u, 2u) == self.get(3u, 3u) {
+                match self.get(1u, 1u) {
+                    Ok(X) => return true,
+                    Ok(O) => return true,
+                    _ => (),
+                }
+            }
+
+            if self.get(1u, 3u) == self.get(2u, 2u)
+            && self.get(2u, 2u) == self.get(3u, 1u) {
+                match self.get(1u, 3u) {
+                    Ok(X) => return true,
+                    Ok(O) => return true,
+                    _ => (),
+                }
+            }
+
             false
-        } else {
-            self.state[x-1][y-1] = c;
+        }
+
+        pub fn is_over(&self) -> bool {
+            if self.is_won() {
+                return true;
+            }
+
+            for i in range(1u, 4u) {
+                for j in range(1u, 4u) {
+                    if self.cell_is_avail(i, j) {
+                        return false;
+                    }
+                }
+
+            }
+
             true
         }
     }
+}
 
-    fn print(&self) {
-        println!("{}\n{}\n{}", self.state[0], self.state[1], self.state[2]);
+mod player {
+    use std::rand;
+    use std::rand::distributions::{IndependentSample, Range};
+
+    use game::{Game, Cell, X, O};
+
+    pub enum PlayerType {
+        PX,
+        PO,
     }
 
-    fn is_won(&self) -> bool {
-        for i in range(1u, 4u) {
-            if self.get(i, 1u) == self.get(i, 2u)
-            && self.get(i, 2u) == self.get(i, 3u)
-            && self.get(i, 1u) == self.get(i, 3u) {
-                match self.get(i, 1u) {
-                    Ok(X) => return true,
-                    Ok(O) => return true,
-                    _ => (),
-                }
-            }
-        }
-
-        for i in range(1u, 4u) {
-            if self.get(1u, i) == self.get(2u, i)
-            && self.get(2u, i) == self.get(3u, i)
-            && self.get(1u, i) == self.get(3u, i) {
-                match self.get(1u, i) {
-                    Ok(X) => return true,
-                    Ok(O) => return true,
-                    _ => (),
-                }
-            }
-        }
-
-        if self.get(1u, 1u) == self.get(2u, 2u)
-        && self.get(2u, 2u) == self.get(3u, 3u) {
-            match self.get(1u, 1u) {
-                Ok(X) => return true,
-                Ok(O) => return true,
-                _ => (),
-            }
-        }
-
-        if self.get(1u, 3u) == self.get(2u, 2u)
-        && self.get(2u, 2u) == self.get(3u, 1u) {
-            match self.get(1u, 3u) {
-                Ok(X) => return true,
-                Ok(O) => return true,
-                _ => (),
-            }
-        }
-
-        false
-    }
-
-    fn is_over(&self) -> bool {
-        if self.is_won() {
-            return true;
-        }
-
-        for i in range(1u, 4u) {
-            for j in range(1u, 4u) {
-                if self.cell_is_avail(i, j) {
-                    return false;
+    pub trait Player {
+        fn ptype(&self) -> PlayerType;
+        fn move(&self, game: &mut Game);
+        fn make_move(&self, game: &mut Game, x: uint, y: uint) {
+            fn convert(ptype: PlayerType) -> Cell {
+                match ptype {
+                    PX => X,
+                    _  => O,
                 }
             }
 
+            game.set(x, y, convert(self.ptype()));
         }
 
-        true
     }
-}
 
-trait Player {
-    fn ptype(&self) -> PlayerType;
-    fn move(&self, game: &mut Game);
-    fn make_move(&self, game: &mut Game, x: uint, y: uint) {
-        fn convert(ptype: PlayerType) -> Cell {
-            match ptype {
-                PX => X,
-                _  => O,
+    pub struct RandomPlayer {
+        ptype: PlayerType,
+    }
+
+    impl Player for RandomPlayer {
+        fn ptype(&self) -> PlayerType{
+            self.ptype
+        }
+
+        fn move(&self, game: &mut Game) {
+            let between = Range::new(0u, 9u);
+            let mut rng = rand::task_rng();
+
+            fn convert(x: uint) -> (uint, uint) {
+                (x / 3 + 1, x % 3 + 1)
             }
-        }
 
-        game.set(x, y, convert(self.ptype()));
+            let (mut x, mut y) = convert(between.ind_sample(&mut rng));
+            while !game.cell_is_avail(x, y) {
+                let (newx, newy) = convert(between.ind_sample(&mut rng));
+                x = newx;
+                y = newy;
+            }
+
+            self.make_move(game, x, y);
+        }
     }
 
-}
-
-struct RandomPlayer {
-    ptype: PlayerType,
-}
-
-impl Player for RandomPlayer {
-    fn ptype(&self) -> PlayerType{
-        self.ptype
+    pub struct RLPlayer {
+        ptype: PlayerType,
     }
 
-    fn move(&self, game: &mut Game) {
-        let between = Range::new(0u, 9u);
-        let mut rng = rand::task_rng();
-
-        fn convert(x: uint) -> (uint, uint) {
-            (x / 3 + 1, x % 3 + 1)
+    impl Player for RLPlayer {
+        fn ptype(&self) -> PlayerType{
+            self.ptype
         }
 
-        let (mut x, mut y) = convert(between.ind_sample(&mut rng));
-        while !game.cell_is_avail(x, y) {
-            let (newx, newy) = convert(between.ind_sample(&mut rng));
-            x = newx;
-            y = newy;
-        }
+        fn move(&self, game: &mut Game) {
+            let (x, y) = (1u, 1u); // dummy
 
-        self.make_move(game, x, y);
+
+            self.make_move(game, x, y);
+        }
     }
 }
 
@@ -197,5 +226,4 @@ fn main() {
         println!("----------");
         i += 1;
     }
-
 }
